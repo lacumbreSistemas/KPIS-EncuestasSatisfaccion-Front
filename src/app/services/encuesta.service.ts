b4860ec04ab828ed879732cf9d09478dad314ce2
juansanchez1990 <51333698+juansanchez1990@users.noreply.github.com>
2022-06-21 10:10:21 -0600

    comit

--- src/app/services/encuesta.service.ts
+++ src/app/services/encuesta.service.ts
@@ -5,8 +5,8 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class EncuestaService {
-  //private AppUrl= 'http://localhost:54469/api/encuesta/';
-  private AppUrl= 'http://10.10.1.221:8064/api/encuesta/';
+    private AppUrl= 'http://localhost:54469/api/encuesta/';
+  // private AppUrl= 'http://10.10.1.221:8064/api/encuesta/';
   constructor(private http: HttpClient) { }
   obtenerPreguntas(){
     return this.http.get(this.AppUrl+'ObtenerPreguntas');
@@ -35,7 +35,7 @@ export class EncuestaService {
 
   }
   ActualizarPuntaje(id:any, valoracion:any){
-  
+
     return this.http.put(this.AppUrl+'updateEncuesta'+'/'+id+'/'+valoracion,valoracion);
 
   }
